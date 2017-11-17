b69435417841d8d953750e79312fada70c1d37b7
yuval matityahu <yuval.matityhu@gmail.com>
2017-11-17 05:57:54 +0200

    first commit

---
+++ ex05-10.sh
@@ -0,0 +1,18 @@
+type tac
+alias rm
+man rm
+touch testfile
+rm testfile
+alias City="Modiin"
+set -x City
+set +x
+unalias City
+which cat
+which passwd
+echo ??\\
+echo '??\\'
+echo "??\\\\"
+echo -e "One\n Two\n\ Tree"
+echo Hello $USER
+
+
